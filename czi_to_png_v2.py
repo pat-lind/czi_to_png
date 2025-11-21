e81a9a22a45dbd3e345b4b9f181b12f183338ef6
pat-lind <sazuesbiz@gmail.com>
2025-11-21 13:56:04 -0700

    Update czi_to_png_v2.py
    Update

--- czi_to_png_v2.py
+++ czi_to_png_v2.py
@@ -3,52 +3,45 @@ import argparse
 from pylibCZIrw import czi
 import matplotlib.image
 from skimage.transform import resize
+from czitools.metadata_tools.dimension import CziDimensions
+from czitools.metadata_tools.czi_metadata import CziScaling
+from czitools.metadata_tools.channel import CziChannelInfo
+
 #TODO
 # >>> ADAPT TO 5x/z-stack FILES
-def sequence_conversion(file_name, file_prefix):
-    #expected input "MM 437 LH C10.czi" or "MM 437 LH C1.czi"
-    # or MM 437 LH C11_Final.czi or MM 437 LH C11_Full Tissue.czi or MM 437 LH C11 (large).czi
-    keys = {'A':1,'B':2,'C':3,'D':4} #go up to E
-    postfix = file_name[len(file_prefix):len(file_prefix)+4].strip().replace('.','')
-    postfix_keynumber = keys[postfix[0]]
-    postfix_numbers = int(postfix[1:])*10
-    if postfix_numbers >= 100:
-        new_postfix = '0' + str(postfix_keynumber + postfix_numbers)
-    else:
-        new_postfix = '0' + '0' + str(postfix_keynumber + postfix_numbers)
-    if file_name[len(file_prefix)+4:].strip() == '.czi': #the case of nothing after "C##"
-        new_prefix = file_prefix
-    else:
-        new_prefix = file_prefix + '_' + file_name[len(file_prefix)+4:].strip().replace('.czi','').replace('czi','')
+# >>> ADAPT TO DO ANY CZI FILE WITHOUT DESIGNATING PREFIX BY JUST COPYING FILENAME + CHANNEL NAME
 
-    return new_prefix, new_postfix
 
-def czi_to_image(czi_image_path, output_dir, downsize_factor, file_prefix):
+def czi_to_image(czi_image_path, output_dir, downsize_factor, file_prefix, text_file_name):
     file_name = czi_image_path.split('/')[-1]
-    #Load CZI
 
     with czi.open_czi(czi_image_path) as czi_image:
         print("\nResizing and saving image channels for file: " + file_name)
+
+        # save original dimensions, spacing, and downsize_factor to a text file
+        with open(text_file_name, 'a') as file:
+            spacing = [CziScaling(czi_image_path).X, CziScaling(czi_image_path).Y]
+            dims = f"{file_name},  original dims, {CziDimensions(czi_image_path).SizeX} {CziDimensions(czi_image_path).SizeY},  spacing, {spacing},  downscaling factor, {downsize_factor}\n"
+            file.write(dims)
         #for each channel in czi file
-        image_data = czi_image.metadata["ImageDocument"]["Metadata"]["Information"]["Image"]["Dimensions"]["Channels"]["Channel"]
+        channels = CziChannelInfo(czi_image_path).names
         bounding_box = czi_image.total_bounding_rectangle[2:]
-        for i in range(len(image_data)):#INSTEAD OF czi_dims, maybe just get shape of image_data
-            channel_name = image_data[i]["@Name"].replace(" ","").replace("-","").replace("/","-")
+        for i in range(len(channels)):
+            channel_name = CziChannelInfo(czi_image_path).names[i].replace(" ","").replace("-","").replace("/","-")
+
             print("Resizing and saving image for channel: " + channel_name)
             frame = czi_image.read(plane={'C': i, 'Z': 0, 'T': 0})
 
             img = resize(frame[..., 0], (bounding_box[1] // downsize_factor, bounding_box[0] // downsize_factor), anti_aliasing=True)
-            #img = resize(frame[..., 0], (449, 699), anti_aliasing=True)
 
-            #prefix, postfix = sequence_conversion(file_name, file_prefix)
-            postfix = file_name[len(file_prefix):len(file_prefix)+4].strip().replace('.','')
+            postfix = file_name.split("\\")[-1].strip()[len(file_prefix):].replace('.czi','').replace('czi','').replace(" ", "") #anything after file prefix
             #if sub-folder of channel name exists then save
             #else create sub-folder and save
             if os.path.exists(output_dir + channel_name):
-                matplotlib.image.imsave(output_dir + channel_name + '/' + file_prefix + '_' + channel_name + "_" +  postfix + '.png', img)
+                matplotlib.image.imsave(output_dir + channel_name + '\\' + file_prefix + '_' + channel_name + "_" +  postfix + '.png', img)
             else:
                 os.makedirs(output_dir + channel_name)
-                matplotlib.image.imsave(output_dir + channel_name + '/' + file_prefix + '_' + channel_name + "_" + postfix + '.png', img)
+                matplotlib.image.imsave(output_dir + channel_name + '\\' + file_prefix + '_' + channel_name + "_" + postfix + '.png', img)
 
 
 def run(czi_dir, output_dir, file_prefix, downsize_factor):
@@ -64,7 +57,15 @@ def run(czi_dir, output_dir, file_prefix, downsize_factor):
     #replace each file directory for the file name
     for file in files:
         try:
-            czi_to_image(file, output_dir, downsize_factor, file_prefix)
+            #create a text file or append to an existing one for all czis that are going to be processed
+            text_file_name = output_dir + 'dims.txt'
+
+            if not os.path.exists(text_file_name):  # if file doesn't exist already
+                os.makedirs(output_dir, exist_ok=True) #make parent folder if doesn't exist
+                with open(text_file_name, 'w') as text_file: #make text file
+                    text_file.write("OG CZI INFO:\n\n")
+
+            czi_to_image(file, output_dir, downsize_factor, file_prefix, text_file_name)
         except Exception as e:
             print("Failed to convert file: ", file)
             file_exceptions.append(file)
@@ -80,9 +81,10 @@ def run(czi_dir, output_dir, file_prefix, downsize_factor):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="The output filenames for this script are those adapted for Tward et al histsetup.py"
-                    "Please use an input folder with files that you've hand selected."
-                    "**Z-stacks are not adapted, i.e. do not work yet."
+        description="Please use an input folder with files that you've selected."
+                    "Note the file prefix parameter: this looks at the folder and processes all the files with the same prefix, "
+                    "- then uses this prefix to name the new file. "
+                    "Z-stacks are not yet adapted."
     )
 
     parser.add_argument(
